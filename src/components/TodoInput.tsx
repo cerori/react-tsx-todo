97a99aa682cda0fce667346fe3801162ac7f40ab
박정근 <wjdrmsl@conia.co.kr>
2022-03-04 15:15:19 +0900

    컴포넌트 통신

---
+++ src/components/TodoInput.tsx
@@ -0,0 +1,32 @@
+import styles from '../Todo.module.css';
+import React, { useState } from 'react';
+
+interface Props {
+  readonly onInsert: (value: string) => void;
+}
+
+const TodoInput = ({ onInsert }: Props) => {
+  const [value, setValue] = useState('');
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    onInsert(value);
+    setValue('');
+  };
+
+  return (
+    <div className={styles.input}>
+      <form onSubmit={onSubmit}>
+        <input placeholder="할 일을 입력하세요" value={value} onChange={onChange} />
+        <button type="submit">추가</button>
+      </form>
+    </div>
+  );
+};
+
+export default TodoInput;
